d7b9f747616f83509dd9d7e7200b3d4513f6803a
Shekhar <shekharnath9@gmail.com>
2023-09-11 18:48:15 +0530

    added notebook

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of required libraries
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip('\n') for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+
+setup(
+    name='image-analysis',
+    version='0.0.1',
+    author = 'Shekhar',
+    author_email='shekharnath9@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
